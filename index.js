275c00b0ef34120d53e6ad5fda863a7bdbb5a98d
Mustaque Nadim <mustaquenadim2000@gmail.com>
2021-03-31 16:59:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u704q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('error!', err);
+    const bookCollection = client.db("lighthouse").collection("books");
+
+    app.post('/addBook', (req, res) => {
+        const newBook = req.body;
+        bookCollection.insertOne(newBook)
+        .then(result => {
+            console.log('inserted count', result.insertedCount);
+            res.send(result.insertedCount > 0);
+        })
+    })
+
+    app.get('/books', (req, res) => {
+        bookCollection.find()
+        .toArray((err, books) => {
+            res.send(books);
+        })
+    })
+
+    app.delete('/deleteBook/:id', (req, res) => {
+        const id = ObjectID(req.params.id);
+        bookCollection.findOneAndDelete({_id: id})
+        .then(document => res.send(document.value))
+    })
+
+    // client.close();
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
